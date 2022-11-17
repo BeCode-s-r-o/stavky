3c7da49ead58f1fcb3792e4b0e0f14781c717370
Vladimír Tomko <82036512+VladimirTomko@users.noreply.github.com>
2022-11-17 12:12:39 +0100

    package.json
    update
    
    fix yml
    
    yaml + imageoptim
    
    eslint + prettier
    
    Update
    
    additional scripts + building
    
    typo

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,32 @@
-import { BrowserRouter as Router } from "react-router-dom";
-import PageScrollTop from "./components/pageToTop/PageScrollTop";
+import { BrowserRouter as Router } from 'react-router-dom';
+import PageScrollTop from './components/pageToTop/PageScrollTop';
 
 // Pages import Here
-import WebAgency from "./pages/WebAgency";
+import WebAgency from './pages/WebAgency';
 
 // Elements import Here
 
-import CounterUp from "./elements/counterup/CounterUp";
-import Service from "./elements/service/Service";
+import CounterUp from './elements/counterup/CounterUp';
+import Service from './elements/service/Service';
 
-import RnAccordion from "./elements/accordion/RnAccordion";
-import Pricing from "./elements/pricing/Pricing";
-import Social from "./elements/social/Social";
-import Tab from "./elements/tab/Tab";
-import Testimonial from "./elements/testimonial/Testimonial";
-import Timeline from "./elements/timeline/Timeline";
+import RnAccordion from './elements/accordion/RnAccordion';
+import Pricing from './elements/pricing/Pricing';
+import Social from './elements/social/Social';
+import Tab from './elements/tab/Tab';
+import Testimonial from './elements/testimonial/Testimonial';
+import Timeline from './elements/timeline/Timeline';
 
-import CalltoAction from "./elements/calltoaction/CalltoAction";
+import CalltoAction from './elements/calltoaction/CalltoAction';
 
-import Contact from "./elements/contact/Contact";
+import Contact from './elements/contact/Contact';
 
-import BlogGridView from "./components/blog/BlogGridView";
+import BlogGridView from './components/blog/BlogGridView';
 
 // Import Css Here
-import "./assets/scss/style.scss";
+import './assets/scss/style.scss';
 
-import Copyright from "./common/footer/Copyright";
-import FooterTwo from "./common/footer/FooterTwo";
+import Copyright from './common/footer/Copyright';
+import FooterTwo from './common/footer/FooterTwo';
 
 const App = () => {
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     {process.env.REACT_APP_WEBSITE_TYPE}
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
