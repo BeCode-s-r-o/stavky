4b438afa76cdb23a18a696c4e51b8ffcd0a0c0fb
VladimirTomko <vladimir.tomko@onesimplicity.com>
2022-11-23 11:03:34 +0100

    Seo meta

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,14 @@ import WebAgency from './pages/WebAgency';
 import CounterUp from './elements/counterup/CounterUp';
 import Service from './elements/service/Service';
 
+import { Helmet } from 'react-helmet';
 import RnAccordion from './elements/accordion/RnAccordion';
+import CalltoAction from './elements/calltoaction/CalltoAction';
 import Pricing from './elements/pricing/Pricing';
 import Social from './elements/social/Social';
 import Tab from './elements/tab/Tab';
 import Testimonial from './elements/testimonial/Testimonial';
 import Timeline from './elements/timeline/Timeline';
-
-import CalltoAction from './elements/calltoaction/CalltoAction';
 
 import Contact from './elements/contact/Contact';
 
@@ -26,15 +26,47 @@ import BlogGridView from './components/blog/BlogGridView';
 import './assets/scss/style.scss';
 
 import Copyright from './common/footer/Copyright';
-import FooterTwo from './common/footer/FooterTwo';
-import CalltoActionSix from './elements/calltoaction/CalltoActionSix';
-import HeaderOne from './common/header/HeaderOne';
 import WhyUs from './pages/WhyUs';
 
 const App = () => {
   const pageType = process.env.REACT_APP_WEBSITE_TYPE;
   return (
     <Router>
+      <Helmet>
+        <title>Začnite zarábať stávkovaním zápasov | STÁVKOVÉ PORADENSTVO</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/stavkove/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/stavkove/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/stavkove/favicon-16x16.png" />
+        <link rel="manifest" href="/stavkove/site.webmanifest" />
+        <link rel="mask-icon" href="/stavkove/safari-pinned-tab.svg" color="#3b055b" />
+        <link rel="shortcut icon" href="/stavkove/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-config" content="/stavkove/browserconfig.xml" />
+        <meta name="theme-color" content="#ffffff"></meta>
+        <link rel="canonical" href="https://www.stavkoveporadenstvo.sk" />
+        <meta name="title" content="Začnite zarábať stávkovaním zápasov | STÁVKOVÉ PORADENSTVO" />
+        <meta property="og:title" content="Začnite zarábať stávkovaním zápasov | STÁVKOVÉ PORADENSTVO" />
+        <meta
+          name="description"
+          content="Naša spoločnosť poskytuje odborné poradenstvo a konzultácie v oblasti stávkovania športových zápasov. Sme tím zložený z 5 členov, máme niekoľkoročné skúsenosti v oblasti stávkovania zápasov a taktiež celkový prehľad v športe a investíciách."
+        />
+        <meta
+          property="og:description"
+          content="Naša spoločnosť poskytuje odborné poradenstvo a konzultácie v oblasti stávkovania športových zápasov. Sme tím zložený z 5 členov, máme niekoľkoročné skúsenosti v oblasti stávkovania zápasov a taktiež celkový prehľad v športe a investíciách."
+        />
+
+        <meta name="keywords" content="stavkovanie,investicia" />
+        <meta property="og:site_name" content="Začnite zarábať stávkovaním zápasov | STÁVKOVÉ PORADENSTVO" />
+        <meta property="og:url" content="https://stavkoveporadenstvo.sk" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="https://stavkoveporadenstvo.sk" />
+        <meta name="robots" content="index, follow" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="Slovak" />
+        <meta name="revisit-after" content="7 days" />
+        <meta name="author" content="Stávkové poradenstvo" />
+        <meta name="publisher" content="Stávkové poradenstvo" />
+      </Helmet>
       <main className="page-wrapper" id="main">
         <PageScrollTop>
           <WebAgency pageType={pageType} />
--- src/AppForex.tsx
+++ src/AppForex.tsx
@@ -1,25 +1,61 @@
+import { Helmet } from 'react-helmet';
 import { BrowserRouter as Router } from 'react-router-dom';
+import './assets/scss/style-forex.scss';
+import Copyright from './common/footer/Copyright';
+import BlogGridView from './components/blog/BlogGridView';
 import PageScrollTop from './components/pageToTop/PageScrollTop';
-import WebAgency from './pages/WebAgency';
-import CounterUp from './elements/counterup/CounterUp';
-import Service from './elements/service/Service';
 import RnAccordion from './elements/accordion/RnAccordion';
+import CalltoAction from './elements/calltoaction/CalltoAction';
+import Contact from './elements/contact/Contact';
+import CounterUp from './elements/counterup/CounterUp';
 import Pricing from './elements/pricing/Pricing';
+import Service from './elements/service/Service';
 import Social from './elements/social/Social';
 import Tab from './elements/tab/Tab';
 import Testimonial from './elements/testimonial/Testimonial';
 import Timeline from './elements/timeline/Timeline';
-import CalltoAction from './elements/calltoaction/CalltoAction';
-import Contact from './elements/contact/Contact';
-import BlogGridView from './components/blog/BlogGridView';
-import './assets/scss/style-forex.scss';
-import Copyright from './common/footer/Copyright';
+import WebAgency from './pages/WebAgency';
 import WhyUs from './pages/WhyUs';
 
 const AppForex = () => {
   const pageType = process.env.REACT_APP_WEBSITE_TYPE;
   return (
     <Router>
+      <Helmet>
+        <title>Začnite zarábať forex obchodovaním | FOREX PORADENSTVO</title>
+        <link rel="apple-touch-icon" sizes="152x152" href="/forex/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/forex/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/forex/favicon-16x16.png" />
+        <link rel="manifest" href="/forex/site.webmanifest" />
+        <link rel="mask-icon" href="/forex/safari-pinned-tab.svg" color="#4e51a1" />
+        <link rel="shortcut icon" href="/forex/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#4e51a1" />
+        <meta name="msapplication-config" content="/forex/browserconfig.xml" />
+        <meta name="theme-color" content="#ffffff" />
+        <link rel="canonical" href="https://www.forexporadenstvo.sk" />
+        <meta name="title" content="Začnite zarábať forex obchodovaním | FOREX PORADENSTVO" />
+        <meta property="og:title" content="Začnite zarábať forex obchodovaním | FOREX PORADENSTVO" />
+        <meta
+          name="description"
+          content="Naša spoločnosť poskytuje odborné poradenstvo a konzultácie v oblasti obchodovania menových párov. Sme tím zložený z 5 členov, máme niekoľkoročné skúsenosti v oblasti akciových trhov a taktiež celkový prehľad v ekonomike a investíciách."
+        />
+        <meta
+          property="og:description"
+          content="Naša spoločnosť poskytuje odborné poradenstvo a konzultácie v oblasti obchodovania menových párov. Sme tím zložený z 5 členov, máme niekoľkoročné skúsenosti v oblasti akciových trhov a taktiež celkový prehľad v ekonomike a investíciách."
+        />
+
+        <meta name="keywords" content="stavkovanie,investicia" />
+        <meta property="og:site_name" content="Začnite zarábať forex obchodovaním | FOREX PORADENSTVO" />
+        <meta property="og:url" content="https://forexporadenstvo.sk" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="https://forexporadenstvo.sk" />
+        <meta name="robots" content="index, follow" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="Slovak" />
+        <meta name="revisit-after" content="7 days" />
+        <meta name="author" content="Forex poradenstvo" />
+        <meta name="publisher" content="Forex poradenstvo" />
+      </Helmet>
       <main className="page-wrapper" id="main">
         <PageScrollTop>
           <WebAgency pageType={pageType} />
