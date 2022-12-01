142b2a452a93743d92335765bb9897a3dafb08ab
VladimirTomko <vladimir.tomko@onesimplicity.com>
2022-12-01 23:29:43 +0100

    Fix url

--- client/src/elements/pricing/PricingFive.tsx
+++ client/src/elements/pricing/PricingFive.tsx
@@ -1,31 +1,34 @@
-import axios from "axios";
-import { useState } from "react";
-import { FiCheck } from "react-icons/fi";
-import StripeCheckout from "react-stripe-checkout";
-import { toast } from "react-toastify";
+import axios from 'axios';
+import { useState } from 'react';
+import { FiCheck } from 'react-icons/fi';
+import StripeCheckout from 'react-stripe-checkout';
+import { toast } from 'react-toastify';
 
 const PricingFive = () => {
   const pageType = process.env.REACT_APP_WEBSITE_TYPE;
   const [pricing, setPricing] = useState({
     first: {
-      label: "30 dní",
+      label: '30 dní',
       value: 69.9,
-      id: pageType === "stavkove" ? 2 : 12,
+      id: pageType === 'stavkove' ? 2 : 12,
     },
     second: {
-      label: "90 dní",
+      label: '90 dní',
       value: 219.9,
-      id: pageType === "stavkove" ? 5 : 15,
+      id: pageType === 'stavkove' ? 5 : 15,
     },
     third: {
-      label: "90 dní",
+      label: '90 dní',
       value: 169.9,
-      id: pageType === "stavkove" ? 8 : 18,
+      id: pageType === 'stavkove' ? 8 : 18,
     },
   });
 
+  const isProd = false;
+  const url = isProd ? 'https://api.forexporadenstvo.sk/checkout' : 'http://localhost:5500/checkout';
+
   async function handleToken(token: any, id: number) {
-    const response = await axios.post("http://localhost:5000/checkout", {
+    const response = await axios.post(url, {
       token,
       package: { id: id, quantity: 1 },
     });
@@ -34,12 +37,12 @@ const PricingFive = () => {
       /*       toast.success('Platba prebehla úspešne', {
         theme: 'colored',
       }); */
-      console.log("úspech");
+      console.log('úspech');
     } else {
       /*       toast.error('Platba neprebehla úspešne', {
         theme: 'colored',
       }); */
-      console.log("ooo");
+      console.log('ooo');
     }
   }
 
@@ -50,10 +53,7 @@ const PricingFive = () => {
           <div className="pricing-table-inner">
             <div className="pricing-header">
               <h4 className="title">STARTER</h4>
-              <div
-                className="pricing"
-                style={{ background: "var(--color-primary)" }}
-              >
+              <div className="pricing" style={{ background: 'var(--color-primary)' }}>
                 <div className="price-wrapper">
                   <span className="price">{pricing.first.value}0</span>
                   <span className="currency">€</span>
@@ -64,10 +64,7 @@ const PricingFive = () => {
             <div className="pricing-body">
               <ul className="list-style--1">
                 <li>
-                  <FiCheck />{" "}
-                  {pageType === "forex"
-                    ? "1-3 SIGNÁLY DENNE"
-                    : "1-3 TIPY DENNE"}
+                  <FiCheck /> {pageType === 'forex' ? '1-3 SIGNÁLY DENNE' : '1-3 TIPY DENNE'}
                 </li>
                 <li>
                   <FiCheck /> ÚSPEŠNOSŤ 78-80%
@@ -81,9 +78,7 @@ const PricingFive = () => {
               </ul>
             </div>
             <div className="container w-50 mb-5">
-              <p style={{ textAlign: "center", marginBottom: 0 }}>
-                Vyberte dĺžku:
-              </p>
+              <p style={{ textAlign: 'center', marginBottom: 0 }}>Vyberte dĺžku:</p>
               <select
                 value={JSON.stringify(pricing.first)}
                 onChange={(e) => {
@@ -92,27 +87,27 @@ const PricingFive = () => {
               >
                 <option
                   value={JSON.stringify({
-                    label: "15 dní",
+                    label: '15 dní',
                     value: 39.9,
-                    id: pageType === "stavkove" ? 1 : 11,
+                    id: pageType === 'stavkove' ? 1 : 11,
                   })}
                 >
                   15 dní
                 </option>
                 <option
                   value={JSON.stringify({
-                    label: "30 dní",
+                    label: '30 dní',
                     value: 69.9,
-                    id: pageType === "stavkove" ? 2 : 12,
+                    id: pageType === 'stavkove' ? 2 : 12,
                   })}
                 >
                   30 dní
                 </option>
                 <option
                   value={JSON.stringify({
-                    label: "90 dní",
+                    label: '90 dní',
                     value: 149.9,
-                    id: pageType === "stavkove" ? 3 : 13,
+                    id: pageType === 'stavkove' ? 3 : 13,
                   })}
                 >
                   90 dní
@@ -124,12 +119,12 @@ const PricingFive = () => {
               <StripeCheckout
                 panelLabel="€"
                 currency="EUR"
-                style={{ style: "none" }}
+                style={{ style: 'none' }}
                 stripeKey="pk_test_51LJyhMBelUpwp79FkRfmFxHJJqAM7XTYKMYz3OHNtkvCjiMYvVwV4VPED9lXaP7CWjmq0ALVU3e4qLwKWOpl0UEo00vuAgo7Mt"
                 token={(e) => handleToken(e, pricing.first.id)}
                 amount={pricing.first.value * 100}
                 name={process.env.REACT_APP_WEBSITE_TYPE}
-                description={""}
+                description={''}
                 billingAddress
               >
                 <a className="btn-default btn-border" href="#">
@@ -147,10 +142,7 @@ const PricingFive = () => {
           <div className="pricing-table-inner">
             <div className="pricing-header">
               <h4 className="title">KOMBI</h4>
-              <div
-                className="pricing"
-                style={{ background: "var(--color-pink)" }}
-              >
+              <div className="pricing" style={{ background: 'var(--color-pink)' }}>
                 <div className="price-wrapper">
                   <span className="price">{pricing.second.value}0</span>
                   <span className="currency">€</span>
@@ -167,28 +159,16 @@ const PricingFive = () => {
                   <FiCheck /> LIMITOVANÉ MIESTA
                 </li>
                 <li>
-                  <FiCheck />{" "}
-                  {pageType === "forex"
-                    ? "ZAHRANIČNÉ ZDROJE"
-                    : "VERIFIKOVANÉ ZDROJE"}
+                  <FiCheck /> {pageType === 'forex' ? 'ZAHRANIČNÉ ZDROJE' : 'VERIFIKOVANÉ ZDROJE'}
                 </li>
                 <li>
-                  <FiCheck />{" "}
-                  {pageType === "forex"
-                    ? "5-15 SIGNÁLOV DENNE"
-                    : "5-15 TIPOV DENNE"}
+                  <FiCheck /> {pageType === 'forex' ? '5-15 SIGNÁLOV DENNE' : '5-15 TIPOV DENNE'}
                 </li>
                 <li>
-                  <FiCheck />{" "}
-                  {pageType === "forex"
-                    ? "PRÉMIOVY SIGNÁL ZDARMA"
-                    : "VYSOKÝ KURZ ZDARMA"}
+                  <FiCheck /> {pageType === 'forex' ? 'PRÉMIOVY SIGNÁL ZDARMA' : 'VYSOKÝ KURZ ZDARMA'}
                 </li>
                 <li>
-                  <FiCheck />{" "}
-                  {pageType === "forex"
-                    ? "ÚSPEŠNOSŤ 84-87%"
-                    : "ÚSPEŠNOSŤ 84-86%"}
+                  <FiCheck /> {pageType === 'forex' ? 'ÚSPEŠNOSŤ 84-87%' : 'ÚSPEŠNOSŤ 84-86%'}
                 </li>
                 <li>
                   <FiCheck /> 24/7 SUPPORT
@@ -199,9 +179,7 @@ const PricingFive = () => {
               </ul>
             </div>
             <div className="container w-50 mb-5">
-              <p style={{ textAlign: "center", marginBottom: 0 }}>
-                Vyberte dĺžku:
-              </p>
+              <p style={{ textAlign: 'center', marginBottom: 0 }}>Vyberte dĺžku:</p>
               <select
                 value={JSON.stringify(pricing.second)}
                 onChange={(e) => {
@@ -213,27 +191,27 @@ const PricingFive = () => {
               >
                 <option
                   value={JSON.stringify({
-                    label: "30 dní",
+                    label: '30 dní',
                     value: 129.9,
-                    id: pageType === "stavkove" ? 4 : 14,
+                    id: pageType === 'stavkove' ? 4 : 14,
                   })}
                 >
                   30 dní
                 </option>
                 <option
                   value={JSON.stringify({
-                    label: "90 dní",
+                    label: '90 dní',
                     value: 219.9,
-                    id: pageType === "stavkove" ? 5 : 15,
+                    id: pageType === 'stavkove' ? 5 : 15,
                   })}
                 >
                   90 dní
                 </option>
                 <option
                   value={JSON.stringify({
-                    label: "180 dní",
+                    label: '180 dní',
                     value: 299.9,
-                    id: pageType === "stavkove" ? 6 : 16,
+                    id: pageType === 'stavkove' ? 6 : 16,
                   })}
                 >
                   180 dní
@@ -245,12 +223,12 @@ const PricingFive = () => {
               <StripeCheckout
                 panelLabel="€"
                 currency="EUR"
-                style={{ style: "none" }}
+                style={{ style: 'none' }}
                 stripeKey="pk_test_51LJyhMBelUpwp79FkRfmFxHJJqAM7XTYKMYz3OHNtkvCjiMYvVwV4VPED9lXaP7CWjmq0ALVU3e4qLwKWOpl0UEo00vuAgo7Mt"
                 token={(e) => handleToken(e, pricing.second.id)}
                 amount={pricing.second.value * 100}
                 name={process.env.REACT_APP_WEBSITE_TYPE}
-                description={""}
+                description={''}
                 billingAddress
               >
                 <a className="btn-default btn-border" href="#">
@@ -268,10 +246,7 @@ const PricingFive = () => {
           <div className="pricing-table-inner">
             <div className="pricing-header">
               <h4 className="title">EXCLUSIVE</h4>
-              <div
-                className="pricing"
-                style={{ background: "var(--color-primary)" }}
-              >
+              <div className="pricing" style={{ background: 'var(--color-primary)' }}>
                 <div className="price-wrapper">
                   <span className="price">{pricing.third.value}0</span>
                   <span className="currency">€</span>
@@ -282,10 +257,7 @@ const PricingFive = () => {
             <div className="pricing-body">
               <ul className="list-style--1">
                 <li>
-                  <FiCheck />{" "}
-                  {pageType === "forex"
-                    ? "2-5 SIGNÁLOV DENNE"
-                    : "2-5 TIPOV DENNE"}
+                  <FiCheck /> {pageType === 'forex' ? '2-5 SIGNÁLOV DENNE' : '2-5 TIPOV DENNE'}
                 </li>
                 <li>
                   <FiCheck /> ÚSPEŠNOSŤ 81-83%
@@ -299,9 +271,7 @@ const PricingFive = () => {
               </ul>
             </div>
             <div className="container w-50 mb-5">
-              <p style={{ textAlign: "center", marginBottom: 0 }}>
-                Vyberte dĺžku:
-              </p>
+              <p style={{ textAlign: 'center', marginBottom: 0 }}>Vyberte dĺžku:</p>
               <select
                 value={JSON.stringify(pricing.third)}
                 onChange={(e) => {
@@ -310,27 +280,27 @@ const PricingFive = () => {
               >
                 <option
                   value={JSON.stringify({
-                    label: "30 dní",
+                    label: '30 dní',
                     value: 99.9,
-                    id: pageType === "stavkove" ? 7 : 17,
+                    id: pageType === 'stavkove' ? 7 : 17,
                   })}
                 >
                   30 dní
                 </option>
                 <option
                   value={JSON.stringify({
-                    label: "90 dní",
+                    label: '90 dní',
                     value: 169.9,
-                    id: pageType === "stavkove" ? 8 : 18,
+                    id: pageType === 'stavkove' ? 8 : 18,
                   })}
                 >
                   90 dní
                 </option>
                 <option
                   value={JSON.stringify({
-                    label: "180 dní",
+                    label: '180 dní',
                     value: 239.9,
-                    id: pageType === "stavkove" ? 9 : 19,
+                    id: pageType === 'stavkove' ? 9 : 19,
                   })}
                 >
                   180 dní
@@ -342,12 +312,12 @@ const PricingFive = () => {
               <StripeCheckout
                 panelLabel="€"
                 currency="EUR"
-                style={{ style: "none" }}
+                style={{ style: 'none' }}
                 stripeKey="pk_test_51LJyhMBelUpwp79FkRfmFxHJJqAM7XTYKMYz3OHNtkvCjiMYvVwV4VPED9lXaP7CWjmq0ALVU3e4qLwKWOpl0UEo00vuAgo7Mt"
                 token={(e) => handleToken(e, pricing.third.id)}
                 amount={pricing.third.value * 100}
                 name={process.env.REACT_APP_WEBSITE_TYPE}
-                description={""}
+                description={''}
                 billingAddress
               >
                 <a className="btn-default btn-border" href="#pricing">
