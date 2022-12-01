9114030ed39fc2b1cbaa47e6a2d2770dc4ed1b60
Vladimír Tomko <82036512+VladimirTomko@users.noreply.github.com>
2022-12-01 22:50:53 +0100

    Fix all

---
+++ client/src/elements/pricing/PricingFive.tsx
@@ -0,0 +1,365 @@
+import axios from "axios";
+import { useState } from "react";
+import { FiCheck } from "react-icons/fi";
+import StripeCheckout from "react-stripe-checkout";
+import { toast } from "react-toastify";
+
+const PricingFive = () => {
+  const pageType = process.env.REACT_APP_WEBSITE_TYPE;
+  const [pricing, setPricing] = useState({
+    first: {
+      label: "30 dní",
+      value: 69.9,
+      id: pageType === "stavkove" ? 2 : 12,
+    },
+    second: {
+      label: "90 dní",
+      value: 219.9,
+      id: pageType === "stavkove" ? 5 : 15,
+    },
+    third: {
+      label: "90 dní",
+      value: 169.9,
+      id: pageType === "stavkove" ? 8 : 18,
+    },
+  });
+
+  async function handleToken(token: any, id: number) {
+    const response = await axios.post("http://localhost:5000/checkout", {
+      token,
+      package: { id: id, quantity: 1 },
+    });
+
+    if (response.status === 200) {
+      /*       toast.success('Platba prebehla úspešne', {
+        theme: 'colored',
+      }); */
+      console.log("úspech");
+    } else {
+      /*       toast.error('Platba neprebehla úspešne', {
+        theme: 'colored',
+      }); */
+      console.log("ooo");
+    }
+  }
+
+  return (
+    <div className="row row--15 align-items-center">
+      <div className="col-lg-4 col-md-6 col-12">
+        <div className="rn-pricing style-5 ">
+          <div className="pricing-table-inner">
+            <div className="pricing-header">
+              <h4 className="title">STARTER</h4>
+              <div
+                className="pricing"
+                style={{ background: "var(--color-primary)" }}
+              >
+                <div className="price-wrapper">
+                  <span className="price">{pricing.first.value}0</span>
+                  <span className="currency">€</span>
+                </div>
+                <span className="subtitle">EUR za {pricing.first.label}</span>
+              </div>
+            </div>
+            <div className="pricing-body">
+              <ul className="list-style--1">
+                <li>
+                  <FiCheck />{" "}
+                  {pageType === "forex"
+                    ? "1-3 SIGNÁLY DENNE"
+                    : "1-3 TIPY DENNE"}
+                </li>
+                <li>
+                  <FiCheck /> ÚSPEŠNOSŤ 78-80%
+                </li>
+                <li>
+                  <FiCheck /> 24/7 SUPPORT
+                </li>
+                <li>
+                  <FiCheck /> TELEGRAM NOTIFIKÁCIE
+                </li>
+              </ul>
+            </div>
+            <div className="container w-50 mb-5">
+              <p style={{ textAlign: "center", marginBottom: 0 }}>
+                Vyberte dĺžku:
+              </p>
+              <select
+                value={JSON.stringify(pricing.first)}
+                onChange={(e) => {
+                  setPricing({ ...pricing, first: JSON.parse(e.target.value) });
+                }}
+              >
+                <option
+                  value={JSON.stringify({
+                    label: "15 dní",
+                    value: 39.9,
+                    id: pageType === "stavkove" ? 1 : 11,
+                  })}
+                >
+                  15 dní
+                </option>
+                <option
+                  value={JSON.stringify({
+                    label: "30 dní",
+                    value: 69.9,
+                    id: pageType === "stavkove" ? 2 : 12,
+                  })}
+                >
+                  30 dní
+                </option>
+                <option
+                  value={JSON.stringify({
+                    label: "90 dní",
+                    value: 149.9,
+                    id: pageType === "stavkove" ? 3 : 13,
+                  })}
+                >
+                  90 dní
+                </option>
+              </select>
+            </div>
+            <div className="pricing-footer">
+              {/* @ts-ignore */}
+              <StripeCheckout
+                panelLabel="€"
+                currency="EUR"
+                style={{ style: "none" }}
+                stripeKey="pk_test_51LJyhMBelUpwp79FkRfmFxHJJqAM7XTYKMYz3OHNtkvCjiMYvVwV4VPED9lXaP7CWjmq0ALVU3e4qLwKWOpl0UEo00vuAgo7Mt"
+                token={(e) => handleToken(e, pricing.first.id)}
+                amount={pricing.first.value * 100}
+                name={process.env.REACT_APP_WEBSITE_TYPE}
+                description={""}
+                billingAddress
+              >
+                <a className="btn-default btn-border" href="#">
+                  Objednať
+                </a>
+              </StripeCheckout>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* End PRicing Table Area  */}
+      {/* Start PRicing Table Area  */}
+      <div className="col-lg-4 col-md-6 col-12">
+        <div className="rn-pricing style-5 active">
+          <div className="pricing-table-inner">
+            <div className="pricing-header">
+              <h4 className="title">KOMBI</h4>
+              <div
+                className="pricing"
+                style={{ background: "var(--color-pink)" }}
+              >
+                <div className="price-wrapper">
+                  <span className="price">{pricing.second.value}0</span>
+                  <span className="currency">€</span>
+                </div>
+                <span className="subtitle">EUR za {pricing.second.label}</span>
+              </div>
+            </div>
+            <div className="pricing-body">
+              <ul className="list-style--1">
+                <li>
+                  <FiCheck /> STARTER & EXCLUSIVE
+                </li>
+                <li>
+                  <FiCheck /> LIMITOVANÉ MIESTA
+                </li>
+                <li>
+                  <FiCheck />{" "}
+                  {pageType === "forex"
+                    ? "ZAHRANIČNÉ ZDROJE"
+                    : "VERIFIKOVANÉ ZDROJE"}
+                </li>
+                <li>
+                  <FiCheck />{" "}
+                  {pageType === "forex"
+                    ? "5-15 SIGNÁLOV DENNE"
+                    : "5-15 TIPOV DENNE"}
+                </li>
+                <li>
+                  <FiCheck />{" "}
+                  {pageType === "forex"
+                    ? "PRÉMIOVY SIGNÁL ZDARMA"
+                    : "VYSOKÝ KURZ ZDARMA"}
+                </li>
+                <li>
+                  <FiCheck />{" "}
+                  {pageType === "forex"
+                    ? "ÚSPEŠNOSŤ 84-87%"
+                    : "ÚSPEŠNOSŤ 84-86%"}
+                </li>
+                <li>
+                  <FiCheck /> 24/7 SUPPORT
+                </li>
+                <li>
+                  <FiCheck /> TELEGRAM NOTIFIKÁCIE
+                </li>
+              </ul>
+            </div>
+            <div className="container w-50 mb-5">
+              <p style={{ textAlign: "center", marginBottom: 0 }}>
+                Vyberte dĺžku:
+              </p>
+              <select
+                value={JSON.stringify(pricing.second)}
+                onChange={(e) => {
+                  setPricing({
+                    ...pricing,
+                    second: JSON.parse(e.target.value),
+                  });
+                }}
+              >
+                <option
+                  value={JSON.stringify({
+                    label: "30 dní",
+                    value: 129.9,
+                    id: pageType === "stavkove" ? 4 : 14,
+                  })}
+                >
+                  30 dní
+                </option>
+                <option
+                  value={JSON.stringify({
+                    label: "90 dní",
+                    value: 219.9,
+                    id: pageType === "stavkove" ? 5 : 15,
+                  })}
+                >
+                  90 dní
+                </option>
+                <option
+                  value={JSON.stringify({
+                    label: "180 dní",
+                    value: 299.9,
+                    id: pageType === "stavkove" ? 6 : 16,
+                  })}
+                >
+                  180 dní
+                </option>
+              </select>
+            </div>
+            <div className="pricing-footer">
+              {/* @ts-ignore */}
+              <StripeCheckout
+                panelLabel="€"
+                currency="EUR"
+                style={{ style: "none" }}
+                stripeKey="pk_test_51LJyhMBelUpwp79FkRfmFxHJJqAM7XTYKMYz3OHNtkvCjiMYvVwV4VPED9lXaP7CWjmq0ALVU3e4qLwKWOpl0UEo00vuAgo7Mt"
+                token={(e) => handleToken(e, pricing.second.id)}
+                amount={pricing.second.value * 100}
+                name={process.env.REACT_APP_WEBSITE_TYPE}
+                description={""}
+                billingAddress
+              >
+                <a className="btn-default btn-border" href="#">
+                  Objednať
+                </a>
+              </StripeCheckout>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* End PRicing Table Area  */}
+      {/* Start PRicing Table Area  */}
+      <div className="col-lg-4 col-md-6 col-12">
+        <div className="rn-pricing style-5">
+          <div className="pricing-table-inner">
+            <div className="pricing-header">
+              <h4 className="title">EXCLUSIVE</h4>
+              <div
+                className="pricing"
+                style={{ background: "var(--color-primary)" }}
+              >
+                <div className="price-wrapper">
+                  <span className="price">{pricing.third.value}0</span>
+                  <span className="currency">€</span>
+                </div>
+                <span className="subtitle">EUR za {pricing.third.label}</span>
+              </div>
+            </div>
+            <div className="pricing-body">
+              <ul className="list-style--1">
+                <li>
+                  <FiCheck />{" "}
+                  {pageType === "forex"
+                    ? "2-5 SIGNÁLOV DENNE"
+                    : "2-5 TIPOV DENNE"}
+                </li>
+                <li>
+                  <FiCheck /> ÚSPEŠNOSŤ 81-83%
+                </li>
+                <li>
+                  <FiCheck /> 24/7 SUPPORT
+                </li>
+                <li>
+                  <FiCheck /> TELEGRAM NOTIFIKÁCIE
+                </li>
+              </ul>
+            </div>
+            <div className="container w-50 mb-5">
+              <p style={{ textAlign: "center", marginBottom: 0 }}>
+                Vyberte dĺžku:
+              </p>
+              <select
+                value={JSON.stringify(pricing.third)}
+                onChange={(e) => {
+                  setPricing({ ...pricing, third: JSON.parse(e.target.value) });
+                }}
+              >
+                <option
+                  value={JSON.stringify({
+                    label: "30 dní",
+                    value: 99.9,
+                    id: pageType === "stavkove" ? 7 : 17,
+                  })}
+                >
+                  30 dní
+                </option>
+                <option
+                  value={JSON.stringify({
+                    label: "90 dní",
+                    value: 169.9,
+                    id: pageType === "stavkove" ? 8 : 18,
+                  })}
+                >
+                  90 dní
+                </option>
+                <option
+                  value={JSON.stringify({
+                    label: "180 dní",
+                    value: 239.9,
+                    id: pageType === "stavkove" ? 9 : 19,
+                  })}
+                >
+                  180 dní
+                </option>
+              </select>
+            </div>
+            <div className="pricing-footer">
+              {/* @ts-ignore */}
+              <StripeCheckout
+                panelLabel="€"
+                currency="EUR"
+                style={{ style: "none" }}
+                stripeKey="pk_test_51LJyhMBelUpwp79FkRfmFxHJJqAM7XTYKMYz3OHNtkvCjiMYvVwV4VPED9lXaP7CWjmq0ALVU3e4qLwKWOpl0UEo00vuAgo7Mt"
+                token={(e) => handleToken(e, pricing.third.id)}
+                amount={pricing.third.value * 100}
+                name={process.env.REACT_APP_WEBSITE_TYPE}
+                description={""}
+                billingAddress
+              >
+                <a className="btn-default btn-border" href="#pricing">
+                  Objednať
+                </a>
+              </StripeCheckout>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* End PRicing Table Area  */}
+    </div>
+  );
+};
+export default PricingFive;
---
+++ server/server.js
@@ -0,0 +1,111 @@
+require("dotenv").config();
+const express = require("express");
+const bodyparser = require("body-parser");
+const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
+
+const uuid = require("uuid").v4;
+const fs = require("fs");
+const cors = require("cors");
+const { text } = require("express");
+
+const sha1 = require("js-sha1");
+
+const nodemailer = require("nodemailer");
+const app = express();
+
+app.use(cors());
+
+app.use(bodyparser.urlencoded({ extended: false }));
+
+app.use(bodyparser.json());
+
+const PORT = process.env.PORT || 5000;
+
+const storeItems = new Map([
+  [1, { priceInCents: 3990, name: "Stavkove Starter 15" }],
+  [2, { priceInCents: 6990, name: "Stavkove Starter 30" }],
+  [3, { priceInCents: 14990, name: "Stavkove Starter 90" }],
+  [4, { priceInCents: 12990, name: "Stavkove Kobmi 30" }],
+  [5, { priceInCents: 21990, name: "Stavkove Kobmi 90" }],
+  [6, { priceInCents: 29990, name: "Stavkove Kobmi 180" }],
+  [7, { priceInCents: 9900, name: "Stavkove Exclusive 30" }],
+  [8, { priceInCents: 16990, name: "Stavkove Exclusive 90" }],
+  [9, { priceInCents: 23990, name: "Stavkove Exclusive 180" }],
+  [11, { priceInCents: 3990, name: "Forex Starter 15" }],
+  [12, { priceInCents: 6990, name: "Forex Starter 30" }],
+  [13, { priceInCents: 14990, name: "Forex Starter 90" }],
+  [14, { priceInCents: 12990, name: "Forex Kombi 30" }],
+  [15, { priceInCents: 21990, name: "Forex Kombi 90" }],
+  [16, { priceInCents: 29990, name: "Forex Kombi 180" }],
+  [17, { priceInCents: 9900, name: "Forex Exclusive 30" }],
+  [18, { priceInCents: 16990, name: "Forex Exclusive 90" }],
+  [19, { priceInCents: 23990, name: "Forex Exclusive 180" }],
+]);
+// post request
+app.post("/checkout", async (req, res) => {
+  let error, status;
+
+  try {
+    const { package, token } = req.body;
+    const customer = await stripe.customers.create({
+      email: token.email,
+      source: token.id,
+    });
+
+    const storeItem = storeItems.get(package.id);
+    console.log(storeItem);
+    const key = uuid();
+    const charge = await stripe.charges.create({
+      amount: storeItem.priceInCents,
+      currency: "eur",
+      customer: customer.id,
+      receipt_email: token.email,
+      description: storeItem.name,
+      key: key,
+    });
+    /*     async function sendEmail() {
+      // Generate test SMTP service account from ethereal.email
+      // Only needed if you don't have a real mail account for testing
+
+      // create reusable transporter object using the default SMTP transport
+      let transporter = nodemailer.createTransport({
+        host: "smtp.m1.websupport.sk",
+        port: 465,
+        secure: true, // true for 465, false for other ports
+        auth: {
+          user: "yama@becode.sk", // generated ethereal user
+          pass: process.env.ACOUNT_PASSWORD, // generated ethereal password
+        },
+      });
+
+      // send mail with defined transport object
+      let info = await transporter.sendMail({
+        from: '"KLUB YAMA" <yama@becode.sk>', // sender address
+        to: token.email, // list of receivers
+        subject: "Vaša Objednávka", // Subject line
+        text: `Dobrý deň ${token.card.name}, 
+        
+<--Text musí začinať tu aby si mal soravne odsadenie`, // plain text body
+        attachments: "Tu pridaj fakturu",
+      });
+
+      console.log("Message sent: %s", info.messageId);
+      // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    }
+
+    sendEmail().catch(console.error); */
+
+    status = "success";
+
+    console.log("success");
+  } catch (error) {
+    console.log(error);
+    status = "failure";
+    return;
+  }
+
+  res.json({ error, status });
+});
+app.listen(PORT, () => {
+  console.log("listening");
+});
