f164a2f77b39d3de9afda8ab30946cec879673dd
VladimirTomko <info@becode.sk>
2023-05-08 11:04:03 +0200

    mail

--- client/src/common/footer/Copyright.tsx
+++ client/src/common/footer/Copyright.tsx
@@ -2,29 +2,10 @@ import React from 'react';
 
 const Copyright = ({ pageType }: PageType) => {
   const websiteHref = pageType === 'forex' ? 'www.forexporadenstvo.sk' : 'www.stavkoveporadenstvo.sk';
-
+  const mail = pageType === 'forex' ? 'info@forexporadenstvo.sk' : 'info@stavkoveporadenstvo.sk';
   return (
     <div className="copyright-area copyright-style-one">
       <div className="container">
-        <div className="text-sm">
-          <p style={{ fontSize: '1.1rem', textAlign: 'justify' }}>
-            Po zakúpení služby užívateľ súhlasí s aktívnym predplatným, ktoré môže kedykoľvek zrušiť. Zásady ochrany
-            osobných údajov sú bližšie špecifikované v Spracovanie osobných údajov na stránke{' '}
-            <a href={websiteHref} target="_blank">
-              {websiteHref}
-            </a>{' '}
-            . Všeobecné obchodné podmienky sú bližšie špecifikované v Obchodné podmienky na{' '}
-            <a href={websiteHref} target="_blank">
-              {websiteHref}
-            </a>{' '}
-            . Členstvo je automaticky obnovované podľa zakúpenej služby, aktuálne je 30 / 90 / 180 dňové členstvo. Pri
-            zakúpení členstva na 30/90/180 dní bude automaticky po 30/60/180 dňoch obnovené členstvo, pokiaľ užívateľ
-            nepožiada o ukončenie predplatného pred uhradeným ďalšej platby. Pokiaľ užívateľ žiada o ukončenie
-            predplateného a zamedzeniu ďalších poplatkov, musí kontaktovať poskytovateľa pred ďalším dátumom úhrady
-            členstva na mail a požiadať o ukončenie predplatného, poskytovateľ následne najneskôr do 24 hodín musí
-            ukončiť predplatné a tým zamedziť ďalšiemu obnoveniu členstva.
-          </p>
-        </div>
         <div className="row align-items-center">
           <div className="col-lg-6 col-md-8 col-sm-12 col-12">
             <div className="copyright-left">
@@ -59,7 +40,6 @@ const Copyright = ({ pageType }: PageType) => {
               </p>
             </div>
           </div>
-
           <p style={{ fontSize: '1.1rem', textAlign: 'justify', marginTop: '20px', padding: '0 20px' }}>
             Zásady ochrany osobných údajov sú bližšie špecifikované v Spracovanie osobných údajov na stránke{' '}
             <a href={websiteHref}>{websiteHref}</a> Všeobecné obchodné podmienky sú bližšie špecifikované v Obchodné
@@ -68,7 +48,8 @@ const Copyright = ({ pageType }: PageType) => {
             užívateľ nepožiada o ukončenie predplatného pred uhradeným ďalšej platby, automaticky mu bude členstvo
             predĺžené. Pokiaľ užívateľ žiada o ukončenie predplateného a zamedzeniu ďalších poplatkov, musí kontaktovať
             poskytovateľa pred ďalším dátumom úhrady členstva na mail a požiadať o ukončenie predplatného, poskytovateľ
-            následne najneskôr do 24 hodín musí ukončiť predplatné a tým zamedziť ďalšiemu obnoveniu členstva.
+            následne najneskôr do 24 hodín musí ukončiť predplatné a tým zamedziť ďalšiemu obnoveniu členstva. Kontakt:{' '}
+            {mail}
           </p>
         </div>
       </div>
